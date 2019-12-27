a3a48d975cf3eea2d4ea921b70974ef3127802fb
Tilen Majerle <tilen.majerle@gmail.com>
2019-12-27 21:25:19 +0100

    Update copyright name

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import subprocess, os
 # -- Project information -----------------------------------------------------
 
 project = 'Documentation'
-copyright = '2019, Tilen Majerle'
+copyright = '2019, Tilen MAJERLE'
 author = 'Tilen MAJERLE'
 
 # The full version, including alpha/beta/rc tags
